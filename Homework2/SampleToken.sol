a1fe2f7ae7378d5eaacbbaf771b04c2f8e5f345b
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2022-12-29 15:49:00 +0200

    Improved code

--- Homework2/SampleToken.sol
+++ Homework2/SampleToken.sol
@@ -3,86 +3,116 @@
 pragma solidity ^0.8.0;
 
 contract SampleToken {
-    
     string public name = "Sample Token";
     string public symbol = "TOK";
 
     uint256 private _totalSupply;
-    uint256 private totalSales;
+    uint256 private _totalSales;
+
     address owner;
     address sampleTokenSaleAddress;
-    
-    event Transfer(address indexed _from,
-                   address indexed _to,
-                   uint256 _value);
 
-    event Approval(address indexed _owner,
-                   address indexed _spender,
-                   uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    mapping (address => uint256) private balances;
-    mapping (address => mapping(address => uint256)) private _allowance;
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-    constructor (uint256 _initialSupply) {
-        balances[msg.sender] = _initialSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowance;
+
+    modifier hasEnoughMoney(address _from, uint256 _value) {
+        require(_balances[_from] >= _value, "Not enough money 1");
+        _;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Not owner 1");
+        _;
+    }
+
+    constructor(uint256 _initialSupply) {
+        _balances[msg.sender] = _initialSupply;
         _totalSupply = _initialSupply;
         totalSales = 0;
         owner = msg.sender;
+        sampleTokenSaleAddress = msg.sender;
+
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
-        return balances[_owner];
+        return _balances[_owner];
     }
 
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value);
+    function transfer(address _to, uint256 _value)
+        public
+        hasEnoughMoney(msg.sender, _value)
+        returns (bool success)
+    {
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
 
-        emit Transfer(msg.sender, _to, _value);
-        balances[msg.sender] -= _value;
-        balances[_to] += _value;
         mint(_value);
+        emit Transfer(msg.sender, _to, _value);
+
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        emit Approval(msg.sender, _spender, _value);
+    function approve(address _spender, uint256 _value)
+        public
+        hasEnoughMoney(msg.sender, _value)
+        returns (bool success)
+    {
         _allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
-    function allowance(address _owner,address spender) public view returns (uint256) {
-        return _allowance[_owner][spender];
+    function allowance(address _owner, address _spender)
+        public
+        view
+        returns (uint256)
+    {
+        return _allowance[_owner][_spender];
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= balances[_from]);
-        require(_value <= _allowance[_from][msg.sender]);
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public hasEnoughMoney(_from, _value) returns (bool success) {
+        require(_value <= _allowance[_from][msg.sender], "Not enough approved 1");
+
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
+        _allowance[_from][msg.sender] -= _value;
 
         emit Transfer(_from, _to, _value);
-        balances[_from] -= _value;
-        balances[_to] += _value;
-        _allowance[_from][msg.sender] -= _value;
         return true;
     }
 
-    function setTokenSaleAddress(address tokenSaleAddress) public {
-        require(msg.sender == owner);
-        sampleTokenSaleAddress = tokenSaleAddress;
+    function setTokenSaleAddress(address _tokenSaleAddress) external isOwner {
+        sampleTokenSaleAddress = _tokenSaleAddress;
     }
 
-    function mint(uint256 value) private {
-        totalSales += value;
-        while (totalSales > 10000) {
-            totalSales -= 10000;
+    function mint(uint256 _value) private {
+        _totalSales += _value;
+
+        while (_totalSales > 10000) {
+            _totalSales -= 10000;
             _totalSupply += 1;
-            balances[owner] += 1;
+            _balances[owner] += 1;
             _allowance[owner][sampleTokenSaleAddress] += 1;
+
+            emit Transfer(address(0), owner, 1);
         }
     }
-
 }
--- Homework2/SampleTokenSale.sol
+++ Homework2/SampleTokenSale.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "./SampleToken.sol";
 
 contract SampleTokenSale {
-    
     SampleToken public tokenContract;
     uint256 public tokenPrice;
     address owner;
@@ -14,32 +13,60 @@ contract SampleTokenSale {
 
     event Sell(address indexed _buyer, uint256 indexed _amount);
 
+    modifier isOwner() {
+        require(msg.sender == owner, "Not owner 2");
+        _;
+    }
+
     constructor(SampleToken _tokenContract, uint256 _tokenPrice) {
         owner = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
 
-    function updateTokenPrice(uint256 _tokenPrice) public {
-        require(msg.sender == owner);
+    function updateTokenPrice(uint256 _tokenPrice) external isOwner {
         tokenPrice = _tokenPrice;
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
         require(msg.value >= _numberOfTokens * tokenPrice, "More money please");
-        uint256 remaining = msg.value - _numberOfTokens * tokenPrice;
-        require(tokenContract.allowance(owner, address(this)) >= _numberOfTokens, "Less tokens in stock");
-        require(tokenContract.balanceOf(owner) >= _numberOfTokens, "Owner is poor");
+
+        require(
+            tokenContract.allowance(owner, address(this)) >= _numberOfTokens,
+            "Less tokens in stock"
+        );
+        require(
+            tokenContract.balanceOf(owner) >= _numberOfTokens,
+            "Owner is poor"
+        );
+
         tokensSold += _numberOfTokens;
-        require(tokenContract.transferFrom(owner, msg.sender, _numberOfTokens));
+        uint256 remaining = msg.value - _numberOfTokens * tokenPrice;
+
+        require(
+            tokenContract.transferFrom(owner, msg.sender, _numberOfTokens),
+            "Transfer to buyer failed"
+        );
         payable(msg.sender).transfer(remaining);
         emit Sell(msg.sender, _numberOfTokens);
     }
 
-    function endSale() public {
-        require(msg.sender == owner);
-        require(tokenContract.transfer(owner, tokenContract.balanceOf(address(this))));
-        require(tokenContract.transferFrom(owner, owner, tokenContract.allowance(owner, address(this))));
+    function endSale() external isOwner {
+        require(
+            tokenContract.transfer(
+                owner,
+                tokenContract.balanceOf(address(this))
+            ),
+            "Transfer to owner failed"
+        );
+        require(
+            tokenContract.transferFrom(
+                owner,
+                owner,
+                tokenContract.allowance(owner, address(this))
+            ),
+            "Transfer to owner from owner failed"
+        );
         payable(msg.sender).transfer(address(this).balance);
     }
 }
